87cf95a5993fddb7f9b68ac1adb2b1e6e3083a3a
Vitalii <vitalii.bondarenko.ua@gmail.com>
2023-10-02 15:47:51 +0300

    Initial commit

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -567,7 +567,6 @@ describe('', () => {
       beforeEach(() => {
         // to prevent Cypress from failing the test on uncaught exception
         cy.once('uncaught:exception', () => false);
-
         page.mockCreate({ statusCode: 503, body: 'Service Unavailable' })
           .as('createRequest');
 
@@ -657,7 +656,6 @@ describe('', () => {
 
         todos.assertCount(6);
         // todos.assertNotLoading(5);
-
         todos.assertNotCompleted(5);
         todos.assertTitle(5, 'Test Todo');
 
